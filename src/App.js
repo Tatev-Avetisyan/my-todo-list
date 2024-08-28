5b354bbd6caf416ef9c47567820aa680e18fb384
Tatevik Avetisyan <karotatev@gmail.com>
2024-08-29 00:37:11 +0400

    Added main structure and main wrapper styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import Main from "./components/Main/Main";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Main />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,7 @@
+import styles from "./main.module.scss";
+
+const Main = () => {
+  return <main className={styles.mainWrapper}> </main>;
+};
+
+export default Main;
