8519125d098647cc9d7e80ead4494469ad233964
Tatevik Avetisyan <karotatev@gmail.com>
2024-09-03 18:12:15 +0400

    Added functionality to display count of done and important todos in the header.

--- src/components/Head/TitleAndCount/Count.jsx
+++ src/components/Head/TitleAndCount/Count.jsx
@@ -1,10 +1,17 @@
 import styles from "./titleAndCount.module.scss";
+import { useContext } from "react";
+import { TodoContext } from "../../../context/TodoContext";
 
 const Count = () => {
+  const { todos } = useContext(TodoContext);
+
+  const doneCount = todos.filter((todo) => todo.done).length;
+  const importantCount = todos.filter((todo) => todo.important).length;
+
   return (
     <div className={styles.count}>
-      <p>Done: 2</p>
-      <p>Important: 2</p>
+      <p>Done: {doneCount}</p>
+      <p>Important: {importantCount}</p>
     </div>
   );
 };
